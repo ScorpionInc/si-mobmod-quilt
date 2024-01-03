6b081297ac16f3e616a7a82ed5aa0b4c1768e910
ScorpionInc <d2dejohn@gmail.com>
2024-01-03 13:06:20 -0500

    Example Item & Block
    Added an example Item & Block

--- src/main/java/io/github/scorpioninc/si_mob_mod/SIMobMod.java
+++ src/main/java/io/github/scorpioninc/si_mob_mod/SIMobMod.java
@@ -1,7 +1,17 @@
 package io.github.scorpioninc.si_mob_mod;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,10 +19,21 @@ public class SIMobMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("SI Mob Mod");
+	public static final Item EXAMPLE_ITEM = new Item(new QuiltItemSettings());
+	public static final Block EXAMPLE_BLOCK = new Block(QuiltBlockSettings.create());
 
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "example_item"), EXAMPLE_ITEM);
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+			entries.addItem(EXAMPLE_ITEM);
+		});
+		Registry.register(Registries.BLOCK, new Identifier(mod.metadata().id(), "example_block"), EXAMPLE_BLOCK);
+		Registry.register(Registries.ITEM, new Identifier(mod.metadata().id(), "example_block"), new BlockItem(EXAMPLE_BLOCK, new QuiltItemSettings()));
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+			entries.addItem(EXAMPLE_BLOCK.asItem());
+		});
 	}
 }
